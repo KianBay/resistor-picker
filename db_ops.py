a2c446017ec392ffeb872bc4571c7757a2f01901
KianBay <59806136+KianBay@users.noreply.github.com>
2022-03-09 23:08:50 +0100

    Added some error handling for db table

--- db_ops.py
+++ db_ops.py
@@ -5,7 +5,7 @@ from resistor_pick import RESISTORS
 class db():
     def __init__(self):
 
-        self.conn = sqlite3.connect('resistors.db')
+        self.conn = sqlite3.connect('resistors.db') #Also creates the db file if none present
         self.cur = self.conn.cursor()
         
 
@@ -13,9 +13,13 @@ class db():
         self.cur.execute("CREATE TABLE lab_resistors (resistor_id INTEGER PRIMARY KEY AUTOINCREMENT, resistance_value FLOAT)")
 
     def add_lab_res(self):
-        for res in RESISTORS:
-            self.cur.execute("INSERT INTO lab_resistors(resistance_value) VALUES(?)",(res,))
-            self.conn.commit()
+        try:
+            for res in RESISTORS:
+                self.cur.execute("INSERT INTO lab_resistors(resistance_value) VALUES(?)",(res,))
+                self.conn.commit()
+        except sqlite3.OperationalError as e: #We run the setup if table doesn't exist yet
+            self.setup()
+
     def get_lab_res(self):
         self.cur.execute("SELECT resistance_value FROM lab_resistors")
         data = self.cur.fetchall()
@@ -29,6 +33,6 @@ class db():
 
 if __name__ == '__main__':
     newdb = db()
-    #newdb.setup()
-    #newdb.add_lab_res()
-    print(newdb.get_lab_res())
+    newdb.setup()
+    newdb.add_lab_res()
+    #print(newdb.get_lab_res())
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import requests
-
+import sqlite3
 BASE_URL = 'http://127.0.0.1:5000/'
