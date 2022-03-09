b9524f6b72f1398d56efde651fb9f5ae297d37fe
KianBay <59806136+KianBay@users.noreply.github.com>
2022-03-09 21:50:24 +0100

    db functionality added
    It actually seems to work. Cool!

--- db_ops.py
+++ db_ops.py
@@ -16,8 +16,19 @@ class db():
         for res in RESISTORS:
             self.cur.execute("INSERT INTO lab_resistors(resistance_value) VALUES(?)",(res,))
             self.conn.commit()
+    def get_lab_res(self):
+        self.cur.execute("SELECT resistance_value FROM lab_resistors")
+        data = self.cur.fetchall()
+        lst = []
+        for entries in data:
+            lst.append(entries[0])
+            lst.sort(reverse=True)
+
+        return lst
+
 
 if __name__ == '__main__':
     newdb = db()
-    newdb.setup()
-    newdb.add_lab_res()
+    #newdb.setup()
+    #newdb.add_lab_res()
+    print(newdb.get_lab_res())
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from flask_restful import Api, Resource, reqparse
 
 
 from resistor_pick import pick, RESISTORS
+from db_ops import db
 
 
 app = Flask('ResistorPicker')
 api = Api(app)
+ #Need to implement a way to check if there is a db in root so we can setup from main
+
+
 
 
 class Resistor(Resource):
     def get(self, target_resistance):
-        data = pick(target_resistance, RESISTORS)
+        dev_db = db()
+        data = pick(target_resistance, dev_db.get_lab_res())
         return data
 
 
